14aebc664d185cb3cdc94b733a71d82ccae7f554
hektah <hektah.course@gmail.com>
2021-10-25 14:05:45 +0530

    first commit

---
+++ cypress/integration/3-flipkart-automation/flipkart.spec.js
@@ -0,0 +1,18 @@
+describe("To automate Flipkart app", () => {
+  before(() => {
+    cy.visit("https://www.flipkart.com/", {
+      timeout: 60000,
+    });
+  });
+
+  it("Verify title of the home page", () => {
+    cy.title().should(
+      "eq",
+      "Online Shopping Site for Mobiles, Electronics, Furniture, Grocery, Lifestyle, Books & More. Best Offers!"
+    );
+  });
+
+  it("Verify title of the home page - partial", () => {
+    cy.title().should("include", "Online Shopping Site");
+  });
+});
